26e42722193beb3512a15904070ab0326c82b043
DennisMelamed <hal.melt1@gmail.com>
2017-02-20 12:22:05 -0600

    added header files

---
+++ include/window.h
@@ -0,0 +1,98 @@
+
+class BasicApplication : public wxApp {
+    public:
+        virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame {
+    protected:
+        wxMenuBar  *menuBar;//main menu bar
+        wxMenu     *fileMenu;//file menu
+        wxMenu	   *lab6Menu;
+        wxMenu	   *lab7Menu;
+        wxToolBar  *toolbar;//tollbar not necessary to use
+        
+        //ROI declarations
+        wxPanel* m_canvas;
+        wxPoint m_currentpoint;
+        wxOverlay m_overlay;
+        bool selecting;
+        
+        int ROI [4];
+        
+        
+        
+        std::vector<wxImage> UndoImages;
+
+        int oldWidth, oldHeight; // save old dimensions
+
+        wxImage loadedImage, origImage, activeImage; // image loaded from file and original placeholder
+
+        /* declear message handler */
+        void OnResetImage(wxCommandEvent & event);
+        void OnInvertImage(wxCommandEvent & event);
+        void OnScaleImage(wxCommandEvent & event);
+        void OnSaveImage(wxCommandEvent & event);
+        void OnConvoluteImage(wxCommandEvent & event);
+        void OnMyFunctionImage(wxCommandEvent & event); //---> To be modified!
+
+        //lab6
+        void OnAddNoiseImage(wxCommandEvent & event);
+        void OnMinFilterImage(wxCommandEvent & event);
+        void OnMaxFilterImage(wxCommandEvent & event);
+        void OnMidFilterImage(wxCommandEvent & event);
+
+        //lab7
+        void OnNegLinearTransformImage(wxCommandEvent & event);
+        void OnLogTransformImage(wxCommandEvent & event);
+        void OnPowerTransformImage(wxCommandEvent & event);
+        void OnRandLookupTableTransformImage(wxCommandEvent & event);
+
+        
+        //ROI methods
+        void OnLeftDown( wxMouseEvent& event );
+        void OnLeftUp( wxMouseEvent& event );
+        void OnMotion( wxMouseEvent& event );
+        bool CheckIfInROI(int x, int y);
+        
+        void Undo(wxCommandEvent & event);
+        
+    public:
+        MyFrame(const wxString title, int xpos, int ypos, int width, int height);
+        virtual ~MyFrame();
+
+        void OnExit(wxCommandEvent & event);
+        void OnOpenFile(wxCommandEvent & event);
+        void OnPaint(wxPaintEvent & event);
+
+        DECLARE_EVENT_TABLE()
+};
+
+enum {
+    EXIT_ID = wxID_HIGHEST + 1,
+    LOAD_FILE_ID,
+    RESET_IMAGE_ID,
+    INVERT_IMAGE_ID,
+    SCALE_IMAGE_ID,
+    SAVE_IMAGE_ID,
+    CONVOLUTE_IMAGE_ID,
+    //lab6
+    NOISE_IMAGE_ID,
+    MIN_FILTER_IMAGE_ID,
+    MAX_FILTER_IMAGE_ID,
+    MID_FILTER_IMAGE_ID,
+    //lab7
+    NEG_LINEAR_TRANSFORM_IMAGE_ID,
+    LOG_TRANSFORM_IMAGE_ID,
+    POWER_TRANSFORM_IMAGE_ID,
+    RAND_LOOKUP_TABLE_TRANSFORM_ID,
+    //
+    MY_IMAGE_ID, //--->To be modified!
+    //ROI
+    LEFT_DOWN_ID,
+    LEFT_UP_ID,
+    MOTION_ID,
+    
+    UNDO_ID
+
+};
